4165a644859f2eb73cb48e76446a39859ba01f8a
Matthias Lübken <mdl@mdl-air.fritz.box>
2017-11-30 16:49:37 +0100

    example with iopipes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-exports.handler = (event, context, callback) => {
+const iopipe = require('iopipe')();
+
+exports.handler = iopipe((event, context, callback) => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     callback(null, {
         statusCode: '200',
-        body: 'Hello world.',
+        body: 'Hello world.3',
     });
-};
+});
